35969145fec8323652d57d7728735a079a433d09
MrRazor22 <senthilvenkat22@gmail.com>
2025-10-04 17:34:23 +0530

    url works

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -10,10 +10,10 @@ class Program
             string query = Console.ReadLine();
 
             // Pick which example to run by uncommenting:
-            // await Example1_Basic.Run(query);
+            await Example1_Basic.Run(query);
             // await Example2_FileStore.Run();
             // await Example3_Directory.Run(); 
-            await Example4_Url.Run(query);
+            // await Example4_Url.Run(query);
             // await Example5_Manual.Run();
         }
     }
